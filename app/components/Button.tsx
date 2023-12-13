606f6ddd477576a410f58de722cdd75f6c80f0e9
Max Hirtenstein <maxhirtens@gmail.com>
2023-12-12 17:09:32 -0800

    added prisma

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,7 @@
 const Button = (props: { text: string; onClick: () => void }) => {
   return (
     <button
-      className="bg-indigo-500 text-white p-2 rounded hover:bg-blue-800 m-2"
+      className="bg-blue-500 text-white p-2 shadow-inner rounded-lg hover:bg-blue-800 m-2"
       type="button"
       style={{
         margin: "10px",
--- app/components/MapBox.tsx
+++ app/components/MapBox.tsx
@@ -8,7 +8,7 @@ import LoadingPage from "../loading";
 import Image from "next/image";
 
 const MapBox = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); // [loading, setLoading
   const [geoClicked, setGeoClicked] = useState(false);
   const [location, setLocation] = useState<{ lat: number; lng: number }>({
     lat: 61.2176,
@@ -18,15 +18,34 @@ const MapBox = () => {
   // get user location with permission from button click
   const getGeo = () => {
     setLoading(true);
+    console.log("loading");
     navigator.geolocation.getCurrentPosition((position) => {
       setLocation({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
       setGeoClicked(true);
+      setLoading(false);
+      console.log("done loading");
     });
-    setLoading(false);
   };
+
+  if (loading) {
+    return (
+      <div>
+        <div className="flex flex-col items-center space-y-12 p-12">
+          <div>
+            <h2 className="text-2xl font-bold text-center text-veryDarkBlue">
+              Your Local Restroom Reviews
+            </h2>
+            <div className="w-[375px] h-[550px] md:w-[500px] md:h-[600px] mt-12 border-4 border-white-500 rounded-xl shadow-md overflow-hidden">
+              <LoadingPage />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   if (!geoClicked) {
     try {
@@ -65,9 +84,6 @@ const MapBox = () => {
   }
 
   try {
-    if (loading) {
-      return <LoadingPage />;
-    }
     return (
       <div>
         <div className="flex flex-col items-center space-y-12 p-12">
