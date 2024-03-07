21d3f1cd749aea6af412c52ed5e18ab3e8a0b159
Max Hirtenstein <maxhirtens@gmail.com>
2024-03-07 13:56:13 -0800

    still trying to fix vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import type { NextAuthOptions } from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
